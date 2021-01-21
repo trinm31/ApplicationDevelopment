95f566ddfe408f2cf64d03f55af9163c1c1692d1
trinm31 <trinmgcd19833@fpt.edu.vn>
2021-01-21 09:01:56 +0700

    Delete connection from tri machine

--- App-Dev/Startup.cs
+++ App-Dev/Startup.cs
@@ -35,7 +35,7 @@ namespace App_Dev
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnectionTri")));
+                    Configuration.GetConnectionString("")));
             services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProviders()  
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddSingleton<IEmailSender, EmailSender>();
