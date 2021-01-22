3edf02a5832e34d0b89f69da85f7f47c0b7097ba
trinm31 <trinmgcd19833@fpt.edu.vn>
2021-01-22 13:34:13 +0700

    Done Categories

--- App-Dev/Startup.cs
+++ App-Dev/Startup.cs
@@ -35,7 +35,7 @@ namespace App_Dev
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnectionVu")));
+                    Configuration.GetConnectionString("DefaultConnectionTri")));
             services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProviders()  
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddSingleton<IEmailSender, EmailSender>();
