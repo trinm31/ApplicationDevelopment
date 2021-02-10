938f172bfe1edf0148787ea8c7df107d98acf986
trinm31 <trinmgcd19833@fpt.edu.vn>
2021-02-10 15:32:19 +0700

    Completed

--- App-Dev/Areas/Authenticated/Controllers/HomeController.cs
+++ App-Dev/Areas/Authenticated/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using App_Dev.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,6 +11,22 @@ namespace App_Dev.Areas.Authenticated.Controllers
         // GET
         public IActionResult Index()
         {
+            if (User.IsInRole(SD.Role_Admin))
+            {
+                return RedirectToAction("Index", "Users", new { area = "Authenticated" });
+            }
+            if (User.IsInRole(SD.Role_Staff))
+            {
+                return RedirectToAction("Index", "Overview", new { area = "Authenticated" });
+            }
+            if (User.IsInRole(SD.Role_Trainee))
+            {
+                return RedirectToAction("Index", "Trainee", new { area = "Authenticated" });
+            }
+            if (User.IsInRole(SD.Role_Trainer))
+            {
+                return RedirectToAction("Index", "Trainer", new { area = "Authenticated" });
+            }
             return View();
         }
     }
